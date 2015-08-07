bc2677d2b333749fdee7a72e568d768d9ad9894c
Diego Sousa <diegors90@gmail.com>
2015-08-06 23:12:16 -0300

    ch02dogDoors-start

---
+++ Chapter2/DogDoor.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter2
+{
+    public class DogDoor
+    {
+        public bool Open { get; private set; }
+
+        public DogDoor()
+        {
+            this.Open = false;
+        }
+
+        public void OpenDor()
+        {
+            System.Console.WriteLine("The dog door opens.");
+            this.Open = true;
+        }
+
+        public void CloseDor()
+        {
+            System.Console.WriteLine("The dog door closes.");
+            this.Open = false;
+        }
+
+
+    }
+}
---
+++ Chapter2/Remote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter2
+{
+    public class Remote
+    {
+        public DogDoor Door { get; private set; }
+
+        public Remote(DogDoor door)
+        {
+            this.Door = door;
+        }
+
+        public void PressButton()
+        {
+            Console.WriteLine("Pressing the remote  control button...");
+            if (Door.Open)
+                Door.CloseDor();
+            else
+                Door.OpenDor();
+        }
+    }
+}
